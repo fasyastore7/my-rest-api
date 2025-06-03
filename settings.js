e07bb0927df5f8f6d83265145008e9fd695b7918
fasyastore7 <fasyamr78@gmail.com>
2025-06-03 19:10:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://ujangsubib:xDSVHvT4A9zwqRgy@cluster0.ltphhwb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "fadluajrin@gmail.com" //email
+global.email_password = "roxp wkza mflh oxts" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
